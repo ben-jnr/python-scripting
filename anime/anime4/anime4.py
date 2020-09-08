1b833d57700e8fd360b1b9fa28d2327b7790f869
ben-jnr <bichubenkuruvilla@gmail.com>
2020-09-08 17:33:31 +0530

    interface changes

--- anime/anime4/anime4.py
+++ anime/anime4/anime4.py
@@ -6,40 +6,36 @@ from clint.textui import progress
 import sys
 session = requests.Session()
 
-if(len(sys.argv) == 4):
-	url = sys.argv[1]
-	#use start > 0 and end > 0 and start <= end
-	start = int(sys.argv[2])
-	end = int(sys.argv[3])	
-	if(start > 0 and end > 0 and start <= end):	
-		for i in range(start,end+1):
-			no = str(i)
-			if(len(no) == 1):
-				final_url = url + "0" + no
-			else:
-				final_url = url + no
-			print("\naccessing url : " + final_url)
-			r = session.get(final_url)
-			soup = BeautifulSoup(r.text, 'html.parser')
-			source = soup.find_all('source')
-			if(source != []):
-				img_link = source[0].get('src')
-				print("starting download - episode : " + no)
-				raw_image = session.get(img_link,stream=True)
-				total_length = int(raw_image.headers['Content-Length'])
-				with open(no + '.mp4', 'wb') as f:
-				    	for c in progress.mill(raw_image.iter_content(chunk_size=1024),expected_size=(total_length/1024) +1):
-				    		if c:
-				    			f.write(c)
-				    			f.flush()
-			else:
-				print("episode not found")
-		print("\nfinished\n")
-	else:
-		print("\nprovide meaningful values\n")
-elif(len(sys.argv) < 4 ):
-	print("\nprovide 3 arguments, use command")
-	print("python3 anime4.py url starting-ep ending-ep\n")
+file_name = ""
+url = ""
+url = input("url\t: ")
+#use start > 0 and end > 0 and start <= end
+start = int(input("start\t: "))
+end = int(input("end\t: "))
+file_name = input("name\t: ")
+if(start > 0 and end > 0 and start <= end):	
+	for i in range(start,end+1):
+		no = str(i)
+		if(len(no) == 1):
+			final_url = url + "0" + no
+		else:
+			final_url = url + no
+		print("\naccessing url : " + final_url)
+		r = session.get(final_url)
+		soup = BeautifulSoup(r.text, 'html.parser')
+		source = soup.find_all('source')
+		if(source != []):
+			img_link = source[0].get('src')
+			print("starting download - episode : " + no)
+			raw_image = session.get(img_link,stream=True)
+			total_length = int(raw_image.headers['Content-Length'])
+			with open(file_name + no + '.mp4', 'wb') as f:
+				for c in progress.mill(raw_image.iter_content(chunk_size=1024),expected_size=(total_length/1024) +1):
+			   		if c:
+			   			f.write(c)
+			   			f.flush()
+		else:
+			print("episode not found")
+	print("\nfinished\n")
 else:
-	print("\nonly 3 arguments allowed, use command")
-	print("python3 anime4.py url starting-ep ending-ep\n")
+	print("\nprovide meaningful values\n")
--- anime/anime4/readme.md
+++ anime/anime4/readme.md
@@ -15,14 +15,16 @@ pip install -r req.txt
 ### execution   
 Run the program as:   
 
-**python3  anime4.py  url  start  end**
+**python3 anime4.py**
 
 example:
 
 ```
-python3 anime4.py "https://4anime.to/gintama-porori-hen-episode-" 2 4
+python3 anime4.py
 ```
 
-This command downloads episodes 2-4 of gintama porori hen.   
-Make sure to **not include episode number in the url**.    
+Enter the url of website, without including the episode number    
+ex : **https://4anime.to/gintama-porori-hen-episode-**     
+Then enter starting episode, ending episode and a desired file_name for the batch.   
+resulting names would be : **file_name1.mp4, file_name2.mp4 , if start=1 and end=2**      
 Wait for the download to finish and Enjoy!!
